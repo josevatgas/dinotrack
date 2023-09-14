07d6eef6ee4cbbabd53d7b53e0b742b2782cc070
DavidMed <neo876@hotmail.com>
2023-09-13 23:27:37 -0500

    Create CRUD workshops, states, cities, brands and refs, create new navList Alerts and submenu

--- Dinotrack.Backend/Data/SeedDb.cs
+++ Dinotrack.Backend/Data/SeedDb.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿
+using Microsoft.EntityFrameworkCore;
 using Dinotrack.Shared.Entities;
 using Dinotrack.Backend.Services;
 using Dinotrack.Shared.Responses;
@@ -21,6 +22,7 @@ namespace Dinotrack.Backend.Data
             await _context.Database.EnsureCreatedAsync();
             await CheckCountriesAsync();
             await CheckBrandsAsync();
+            await CheckWorkshopsAsync();
         }
 
         private async Task CheckBrandsAsync()
@@ -51,18 +53,18 @@ namespace Dinotrack.Backend.Data
                     Name = "Yamaha",
                     Refs = new List<Ref>()
                     {
-                        new Ref() { Name = "DR150"},
-                        new Ref() { Name = "V-STROM 250 SX"},
-                        new Ref() { Name = "GIXXER 150 ABS"},
-                        new Ref() { Name = "GIXXER 150 FI"},
-                        new Ref() { Name = "GIXXER SF 150 ABS"},
-                        new Ref() { Name = "GSX S150 ABS"},
-                        new Ref() { Name = "GSX-R150 ABS"},
-                        new Ref() { Name = "GIXXER 250"},
-                        new Ref() { Name = "GIXXER SF 250"},
-                        new Ref() { Name = "AX4 EVOLUTION EURO 3"},
-                        new Ref() { Name = "AX4 EURO 3"},
-                        new Ref() { Name = "GSXS 1000 GT"},
+                        new Ref() { Name = "V80_2000"},
+                        new Ref() { Name = "V80_2000"},
+                        new Ref() { Name = "Axis_2001"},
+                        new Ref() { Name = "BWS 100_2007"},
+                        new Ref() { Name = "BWS 125_2012"},
+                        new Ref() { Name = "Crypton 115_2010"},
+                        new Ref() { Name = "Crypton 115_2014"},
+                        new Ref() { Name = "FZ8 S_2013"},
+                        new Ref() { Name = "MT07_2014"},
+                        new Ref() { Name = "2015_FAZER"},
+                        new Ref() { Name = "SZ-RR_B391_2016"},
+                        new Ref() { Name = "Tenere_250_2015"},
                     }
                 });
                 _context.Brands.Add(new Brand 
@@ -70,18 +72,18 @@ namespace Dinotrack.Backend.Data
                     Name = "Honda",
                     Refs = new List<Ref>()
                     {
-                        new Ref() { Name = "DR150"},
-                        new Ref() { Name = "V-STROM 250 SX"},
-                        new Ref() { Name = "GIXXER 150 ABS"},
-                        new Ref() { Name = "GIXXER 150 FI"},
-                        new Ref() { Name = "GIXXER SF 150 ABS"},
-                        new Ref() { Name = "GSX S150 ABS"},
-                        new Ref() { Name = "GSX-R150 ABS"},
-                        new Ref() { Name = "GIXXER 250"},
-                        new Ref() { Name = "GIXXER SF 250"},
-                        new Ref() { Name = "AX4 EVOLUTION EURO 3"},
-                        new Ref() { Name = "AX4 EURO 3"},
-                        new Ref() { Name = "GSXS 1000 GT"},
+                        new Ref() { Name = "Honda CB110"},
+                        new Ref() { Name = "Navi"},
+                        new Ref() { Name = "Wave 110s"},
+                        new Ref() { Name = "Honda VFR 1200 XD"},
+                        new Ref() { Name = "CBR 1000RR ABS"},
+                        new Ref() { Name = "Pioneer 1000"},
+                        new Ref() { Name = "PCX 160 ABS 2024"},
+                        new Ref() { Name = "PCX 150 DLX"},
+                        new Ref() { Name = "AFRICA TWIN ADVENTURE SPORTS SE"},
+                        new Ref() { Name = "X-ADV"},
+                        new Ref() { Name = "CB 125F"},
+                        new Ref() { Name = "X-Blade 160"},
                     }
                 });
                 _context.Brands.Add(new Brand 
@@ -277,6 +279,7 @@ namespace Dinotrack.Backend.Data
                 await _context.SaveChangesAsync();
             }
         }
+
 
         private async Task CheckWorkshopsAsync()
         {
