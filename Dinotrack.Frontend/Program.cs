6c2c6874135efe58a7e6a8ff5c06db3e55551e7e
DavidMed <neo876@hotmail.com>
2023-10-09 22:56:47 -0500

    Create pagination, filter, pages, backend users, creation token

--- Dinotrack.Frontend/Program.cs
+++ Dinotrack.Frontend/Program.cs
@@ -8,7 +8,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7102/") });
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7102/") });
 builder.Services.AddScoped<IRepository, Repository>();
 builder.Services.AddSweetAlert2();
 
